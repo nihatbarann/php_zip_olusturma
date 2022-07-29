8e97d08df6419a9965147da8921f7051e2063942
Nihat <ngtb.2125@gmail.com>
2022-07-29 14:46:18 +0300

    zip_php

--- zip.php
+++ zip.php
@@ -1,3 +1,32 @@
+<?php 
+
+class Ziple extends ZipArchive {
+
+
+	protected $zipName;
+
+	 function __construct($isim){
+
+			$this->zipName=$isim;                         		      //oluşturacağımız zip dosyasına isim veriyoruz
+			$this->remove();						//oluşturacağımız bir zip dosyası adında dosya varsa onu silip tekrar aynı isimde yukluyoruz	
+			$this->open($isim.'.zip',ZipArchive::CREATE);                //verdiğimiz isimde zip dosyasını oluşturuyoruz.
+
+	}
+public function veriekle($dosya){ 						//hangi klasoru veya dosyayı zipleyeceksek onu methoda gonderiyoruz
+
+
+			$dizin=glob($dosya.'/*');
+			foreach ($dizin as  $deger) {
+
+			if(is_dir($deger)){
+				$this->veriekle($deger);                   //gonderdiğimiz isim klasorse oluşturuduğumuz method çalışacak
+
+			}
+			elseif(is_file($deger)){ 						//dosya ise ZipArchive de dosyalar için bulunan methodu çağırıyoruz 
+				$this->addFile($deger,$deger);
+						}
+							}
+		 						}
 
 public function remove(){ 
 
